f7c42edd6741e5186f271c420f83380a09979d07
Andrew Qiao <andrewqiao2004@gmail.com>
2022-12-31 14:25:02 -0500

    Add constant data and icons

--- constants/data.js
+++ constants/data.js
@@ -2,6 +2,31 @@ import sqlWorkshop from '../public/img/data/sql-workshop.png';
 import micIcon from '../public/img/data/mic.svg';
 import youtubeIcon from '../public/img/data/youtube.svg';
 import spotifyIcon from '../public/img/data/spotify.svg';
+import bradley from '../public/img/data/bradley.png';
+import usersIcon from '../public/img/icons/users.svg';
+import codeIcon from '../public/img/icons/code.svg';
+import briefcaseIcon from '../public/img/icons/briefcase.svg';
+
+export const ABOUT = [
+  {
+    title: 'Who We Are',
+    description:
+      'A community for those who want to learn and talk about Data Science at the University of Waterloo.',
+    icon: usersIcon,
+  },
+  {
+    title: 'Our Mission',
+    description:
+      'To provide more accessible education in the Data Science industry to students on campus.',
+    icon: codeIcon,
+  },
+  {
+    title: 'What We Do',
+    description:
+      'We host events such as workshops, reading groups, conferences, socials, datathons and more.',
+    icon: briefcaseIcon,
+  },
+];
 
 export const EVENTS = [
   {
@@ -233,5 +258,226 @@ export const RECORDINGS = [
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     link: 'https://www.youtube.com/watch?v=IDTZm9b4yHU',
+  },
+];
+
+export const TEAM = [
+  {
+    subteam: 'Presidents',
+    members: [
+      {
+        name: 'Sebastian Valencia',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+    ],
+  },
+  {
+    subteam: 'Design',
+    members: [
+      {
+        name: 'Sebastian Valencia',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+    ],
+  },
+  {
+    subteam: 'Development',
+    members: [
+      {
+        name: 'Sebastian Valencia',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+    ],
+  },
+  {
+    subteam: 'Education',
+    members: [
+      {
+        name: 'Sebastian Valencia',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Sebastian Valencia',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+      {
+        name: 'Neysa Patel',
+        position: 'Co-President',
+        image: bradley,
+        socials: [
+          { type: 'instagram', link: 'https://youtube.com' },
+          { type: 'linkedin', link: 'https://youtube.com' },
+          { type: 'email', link: 'https://youtube.com' },
+          { type: 'personal website', link: 'https://youtube.com' },
+        ],
+      },
+    ],
   },
 ];
---
+++ pages/team.jsx
@@ -0,0 +1,9 @@
+export default function Team() {
+  return (
+    <>
+      <h1 className="text-center">
+        <span className="h1">Our Team</span>
+      </h1>
+    </>
+  );
+}
