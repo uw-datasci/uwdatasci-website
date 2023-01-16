110a3e24ef6cd3526b8b1260f457434b1361ad55
Andrew Qiao <andrewqiao2004@gmail.com>
2023-01-16 15:35:50 -0500

    Move AboutCard into separate file

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -1,5 +1,4 @@
-import Card from '../UI/Card';
-import Image from 'next/image';
+import AboutCard from '../cards/AboutCard';
 import { ABOUT } from '../../constants/data';
 
 export default function About() {
@@ -10,21 +9,7 @@ export default function About() {
       </h2>
       <div className="grid gap-8 sm:grid-cols-2 lg:gap-12 xl:grid-cols-3">
         {ABOUT.map((card) => (
-          <Card key={card.title} borderRadius="rounded-2xl md:rounded-4xl">
-            <div className="flex flex-col px-7 pt-7 pb-8 md:px-9 md:pt-9 md:pb-12">
-              <Image
-                src={card.icon}
-                alt={card.title}
-                className="no-select-or-drag dark:filter-light-purple filter-purple mb-7 w-9 self-end md:w-12"
-              />
-              <h3 className="mb-2 text-2xl font-bold text-black dark:text-white md:mb-3 md:text-3xl">
-                {card.title}
-              </h3>
-              <p className="leading-relaxed text-purple dark:text-lightPurple">
-                {card.description}
-              </p>
-            </div>
-          </Card>
+          <AboutCard title={card.title} description={card.description} icon={card.icon} key={card.title} />
         ))}
       </div>
     </section>
