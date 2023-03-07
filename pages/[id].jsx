c69e61afaf23d0a21a437a4112720e46e74fd400
Andrew Qiao <andrewqiao2004@gmail.com>
2023-03-06 23:08:28 -0500

    Add URL Shortener

---
+++ pages/[id].jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { getDataOnce } from '../lib/firebase';
+
+export default function LinkShortener({ link }) {
+  useEffect(() => {
+    window.open(link, '_self');
+  }, [link]);
+
+  return <></>;
+}
+
+export async function getStaticPaths() {
+  const result = await getDataOnce('linkShortener');
+  const paths = result.map((item) => {
+    return {
+      params: {
+        id: item.id,
+      },
+    };
+  });
+  console.log(paths);
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+
+  const result = await getDataOnce('linkShortener');
+
+  const item = result.find((item) => item.id === id);
+
+  return { props: { link: item.link } };
+}
