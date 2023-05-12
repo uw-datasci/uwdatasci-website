7b804b69ceb4e4eecce65ee5e4d7e01193013826
alvanli <51011489+alvanli@users.noreply.github.com>
2023-05-11 23:21:52 -0400

    Fix "Extra keys returned from getStaticPaths" error

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,32 +1,27 @@
-import { useEffect } from 'react';
-import { getDataOnce } from '../lib/firebase';
+import { useEffect } from "react";
+import { getDataOnce } from "../lib/firebase";
 
 export default function LinkShortener({ link }) {
   useEffect(() => {
-    window.open(link, '_self');
+    window.open(link, "_self");
   }, [link]);
 
   return <></>;
 }
 
 export async function getStaticPaths() {
-  const result = await getDataOnce('linkShortener');
-  const paths = result.map((item) => {
-    return {
-      params: {
-        id: item.id,
-      },
-    };
-  });
-  console.log(paths);
-
-  return { paths, fallback: false, revalidate: 1 };
+  const result = await getDataOnce("linkShortener");
+  const paths = result.map((item) => ({
+      params: { id: item.id },
+    }
+  ));
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps(context) {
   const { id } = context.params;
 
-  const result = await getDataOnce('linkShortener');
+  const result = await getDataOnce("linkShortener");
 
   const item = result.find((item) => item.id === id);
 
