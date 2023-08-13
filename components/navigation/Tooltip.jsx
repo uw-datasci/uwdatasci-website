ed29e7c3165a056a75f4342e03cc1235780b8fcf
alvanli <51011489+alvanli@users.noreply.github.com>
2023-08-13 14:48:24 -0400

    Added closest lines

--- components/navigation/Tooltip.jsx
+++ components/navigation/Tooltip.jsx
@@ -1,21 +1,67 @@
-export const Tooltip = ({ interactionData }) => {
+import { useEffect } from "react";
+
+export const Tooltip = ({ interactionData, courses, setClosestCourses }) => {
   if (!interactionData) {
+    // setClosestCourses([]);
     return null;
   }
+
+  const euclideanDistance = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Arrays must have the same length");
+    }
+    let dotProductSum = 0;
+    let normASum = 0;
+    let normBSum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      dotProductSum += arr1[i] * arr2[i];
+      normASum += Math.pow(arr1[i], 2);
+      normBSum += Math.pow(arr2[i], 2);
+    }
+    return dotProductSum / (Math.sqrt(normASum) * Math.sqrt(normBSum));
+  };
+
   console.log(interactionData);
 
+  const getIndicesOfSmallestNumbers = (arr, n) => {
+    const indices = [];
+    for (let i = 0; i < n; i++) {
+      let minIndex = 0;
+      for (let j = 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      indices.push(minIndex);
+      arr.splice(minIndex, 1);
+    }
+    return indices;
+  };
+
+  const getClosestCourses = () => {
+    const distances = courses.map((a) =>
+      euclideanDistance(a.sm_sbert, interactionData.obj.sm_sbert)
+    );
+    const smallestN = getIndicesOfSmallestNumbers(distances, 10);
+    const closestCourses = smallestN.map((sm) => courses[sm]);
+    closestCourses.push(interactionData);
+    setClosestCourses(closestCourses);
+    console.log(closestCourses);
+  };
+
+  useEffect(() => {
+    getClosestCourses();
+  }, [interactionData]);
+  
   return (
     <div
-      className="absolute  text-white transform translate-y-1/2"
+      className="absolute translate-y-72 translate-x-2 transform text-white bg-black p-2 rounded-sm"
       style={{
         left: interactionData.xPos,
         top: interactionData.yPos,
-        transform: "translateY(50%)"
       }}
     >
       {interactionData.name}
     </div>
   );
 };
-
-// ml-1 rounded-sm bg-black  p-1
--- pages/flow.jsx
+++ pages/flow.jsx
@@ -24,14 +24,14 @@ export default function Courses({ courses }) {
 }
 
 const getX = (course, use_tfidf = false) => {
-  if (use_tfidf){
+  if (use_tfidf) {
     return course.xs_tfidf[0];
   }
   return course.xs_sbert[0];
 };
 
 const getY = (course, use_tfidf = false) => {
-  if (use_tfidf){
+  if (use_tfidf) {
     return course.xs_tfidf[1];
   }
   return course.xs_sbert[1];
@@ -39,14 +39,15 @@ const getY = (course, use_tfidf = false) => {
 
 const MARGIN = { top: 50, right: 50, bottom: 60, left: 100 };
 const BUBBLE_MIN_SIZE = 1;
-const BUBBLE_MAX_SIZE = 5;
+const BUBBLE_MAX_SIZE = 7;
 
 function CoursesGraph({ courses }) {
   const [hovered, setHovered] = useState(null);
+  const [closestCourses, setClosestCourses] = useState([]);
   const [useTFIDF, setUseTFIDF] = useState(false);
 
-  const width = 700;
-  const height = 700;
+  const width = 1000;
+  const height = 1000;
   const axesRef = useRef(null);
   const boundsWidth = width - MARGIN.right - MARGIN.left;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
@@ -98,40 +99,56 @@ function CoursesGraph({ courses }) {
       .attr("transform", "translate(" + -20 + ",0)")
       .call(yAxisGenerator);
 
-
     d3.selectAll(".x.axis").style("color", "white");
     d3.selectAll(".y.axis").style("color", "white");
   }, [xScale, yScale, boundsHeight, boundsWidth]);
 
   // Build the shapes
-  const allShapes = courses
-    // .sort((a, b) => b.pop - a.pop)
-    .map((d, i) => {
-      return (
-        <circle
-          key={i}
-          r={sizeScale(d.useful)}
-          cx={xScale(getX(d, useTFIDF))}
-          cy={yScale(getY(d, useTFIDF))}
-          opacity={1}
-          // stroke="#e0ac2b"
-          stroke={colorScale(d.course_code)}
-          fill={colorScale(d.course_code)}
-          fillOpacity={0.4}
-          strokeWidth={1}
-          onMouseEnter={() =>
-            setHovered({
-              xPos: xScale(getX(d, useTFIDF)),
-              yPos: yScale(getY(d, useTFIDF)),
-              name: d.code,
-              obj: d,
-            })
-          }
-          onClick={() => window.open(`https://uwflow.com/course/${d.code}`)}
-          onMouseLeave={() => setHovered(null)}
-        />
-      );
-    });
+  const allShapes = courses.map((d, i) => {
+    return (
+      <circle
+        key={i}
+        r={sizeScale(d.useful)}
+        cx={xScale(getX(d, useTFIDF))}
+        cy={yScale(getY(d, useTFIDF))}
+        opacity={1}
+        // stroke="#e0ac2b"
+        stroke={colorScale(d.course_code)}
+        fill={colorScale(d.course_code)}
+        fillOpacity={0.4}
+        strokeWidth={1}
+        onMouseEnter={() =>
+          setHovered({
+            xPos: xScale(getX(d, useTFIDF)),
+            yPos: yScale(getY(d, useTFIDF)),
+            name: d.code,
+            obj: d,
+          })
+        }
+        onClick={() => window.open(`https://uwflow.com/course/${d.code}`)}
+        onMouseLeave={() => {
+          setHovered(null);
+          setClosestCourses([]);
+        }}
+      />
+    );
+  });
+
+  const allLines = closestCourses.slice(0, -1).map((d, i) => {
+    return (
+      <line
+        key={i}
+        r={sizeScale(d.useful)}
+        x1={closestCourses.at(-1).xPos}
+        y1={closestCourses.at(-1).yPos}
+        x2={xScale(getX(d, useTFIDF))}
+        y2={yScale(getY(d, useTFIDF))}
+        opacity={1}
+        stroke="#e0ac2b"
+        strokeWidth={1}
+      />
+    );
+  });
 
   return (
     <div>
@@ -157,6 +174,7 @@ function CoursesGraph({ courses }) {
           transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
         >
           {allShapes}
+          {allLines}
         </g>
         <g
           width={boundsWidth}
@@ -177,7 +195,11 @@ function CoursesGraph({ courses }) {
           marginTop: MARGIN.top,
         }}
       >
-        <Tooltip interactionData={hovered} courses={courses} />
+        <Tooltip
+          interactionData={hovered}
+          courses={courses}
+          setClosestCourses={setClosestCourses}
+        />
       </div>
     </div>
   );
